d1459a51cdb7bd3a9f6bf168c3000225050969b0
VladimirDan <vovaoil2003@gmail.com>
2023-02-25 13:18:55 +0200

    Add handler

--- server.go
+++ server.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"kpi/lab1/handlers"
+	"log"
   )
   
   
-func main() {
-	fmt.Print("Starting server at port")
-}
+  func main(){
+	http.HandleFunc("/time", handlers.TimeHandler)
+	fmt.Print("Starting server at port 8795")
+  
+	if err := http.ListenAndServe(":8795", nil); err != nil {
+		log.Fatal(err)
+	}
+  }
