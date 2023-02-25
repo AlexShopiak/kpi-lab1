f939dd7d29adb6405d6eeffe1ebbcce9f6b640d8
Alex Shopiak <alshop2004@gmail.com>
2023-02-25 12:37:27 +0100

    Format code

--- server.go
+++ server.go
@@ -5,14 +5,14 @@ import (
 	"net/http"
 	"kpi/lab1/handlers"
 	"log"
-  )
+)
   
   
-  func main(){
+func main(){
 	http.HandleFunc("/time", handlers.TimeHandler)
 	fmt.Print("Starting server at port 8795")
-  
+
 	if err := http.ListenAndServe(":8795", nil); err != nil {
 		log.Fatal(err)
 	}
-  }
+}
