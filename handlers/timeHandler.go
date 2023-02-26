fd51a89302c340fcafa22da7b24e6986852df083
Alex Shopiak <alshop2004@gmail.com>
2023-02-26 19:41:49 +0100

    Remove error-causing feature

--- handlers/timeHandler.go
+++ handlers/timeHandler.go
@@ -2,32 +2,29 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
 )
 
-func TimeHandler(w http.ResponseWriter, r *http.Request) {  
-  if r.URL.Path != "/time" {
-    http.Error(w, "Error 404 not found.", http.StatusNotFound)
-    return
-  }
-  
-  if r.Method != "GET" {
-    http.Error(w, "This method is not supported.", http.StatusNotFound)
-    return
-  }
+func TimeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/time" {
+		http.Error(w, "Error 404 not found.", http.StatusNotFound)
+		return
+	}
 
-  fmt.Fprintf(w, "Your request: ")
+	if r.Method != "GET" {
+		http.Error(w, "This method is not supported.", http.StatusNotFound)
+		return
+	}
 
-  realtime := time.Now().Format(time.RFC3339)
-  response := map[string]string{"time": realtime}
+	realtime := time.Now().Format(time.RFC3339)
+	response := map[string]string{"time": realtime}
 
-  w.Header().Set("Content-Type", "application/json")
-  err := json.NewEncoder(w).Encode(response)
-  if err != nil {
-    log.Fatalf("Error while encoding: %s", err)
-    return
-  }
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		log.Fatalf("Error while encoding: %s", err)
+		return
+	}
 }
